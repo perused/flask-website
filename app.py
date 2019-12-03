e27c1070124914b51798050bd45e63f82574006d
Sam <sjac6722@uni.sydney.edu.au>
2019-12-03 17:57:52 +1100

    Made an html page and route for each path on the website.

--- app.py
+++ app.py
@@ -5,7 +5,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("html/index.html")
+    return render_template("/html/index.html")
+
+@app.route("/about")
+def about():
+    return render_template("/html/about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("/html/projects.html")
+
+@app.route("/train_game")
+def train_game():
+    return render_template("/html/train_game.html")
+
+@app.route("/centaur_generator")
+def centaur_generator():
+    return render_template("/html/centaur_generator.html")
+
+@app.route("/in_progress")
+def in_progress():
+    return render_template("/html/in_progress.html")
+
 
 if __name__=="__main__":
     app.run(port=5000, debug=True)
